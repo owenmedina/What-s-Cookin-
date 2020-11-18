4aa6497924984626b8d54b221b19e01266734d97
owenmedina <33887545+owenmedina@users.noreply.github.com>
2020-11-18 10:16:23 -0800

    Merge feature/welcome-page into develop (#1)
    * Add first curve and Constants file and restructure to MVC
    
    * Add second curve
    
    * Add third curve and rename CurveView

---
+++ What's Cookin?/K.swift
@@ -0,0 +1,16 @@
+//
+//  K.swift
+//  What's Cookin?
+//
+//  Created by Owen Medina on 11/17/20.
+//
+
+import Foundation
+import UIKit
+
+struct K {
+    // Colors
+    static let red = UIColor(named: "Red")!
+    static let yellow = UIColor(named: "Yellow")!
+    static let orange = UIColor(named: "Orange")!
+}
