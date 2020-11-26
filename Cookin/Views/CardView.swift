1d8d02d151835cb99ca9b7693960caa7ef72ad6c
Owen Medina <owensmedi@gmail.com>
2020-11-25 18:34:37 -0800

    Add custom card view

---
+++ Cookin/Views/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  Cookin
+//
+//  Created by Owen Medina on 11/24/20.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
